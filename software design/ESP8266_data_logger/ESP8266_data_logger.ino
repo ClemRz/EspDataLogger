6036f63994c5acda334ef252c609ac18ced855d9
cronzon <cronzon@switchfly.com>
2016-11-19 04:00:26 +0000

    Initial commit

---
+++ software design/ESP8266_data_logger/ESP8266_data_logger.ino
@@ -0,0 +1,95 @@
+/*
+    Copyright (C) 2016 Clément Ronzon
+
+    This file is part of EspDataLogger.
+
+    SpotPipe is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <ESP8266WiFi.h>
+#include <Wire.h>
+#include <SI7021.h>
+#include "FS.h"
+#include "structures.h"
+
+#define MICROSEC              1000000L
+#define MILLISEC              1000L
+#define SEC                   1L
+#define MINUTE                (unsigned int) 60L*SEC
+#define HOUR                  (unsigned int) 60L*MINUTE
+#define DAY                   (unsigned long) 24L*HOUR
+
+#define SDA                   13
+#define SCL                   12
+
+#define WAKEUP_RATE           15*MINUTE
+#define FILE_PATH             "/data.csv"
+
+#define DEBUG                 0        
+#define RETRIEVE              0
+#define DELETE                0
+
+#define VERSION               "1.0";
+
+SI7021 _sensor;
+
+bool _sensorExists = false;
+     
+int _celsiusHundredths,
+    _humidityPercent;
+
+
+void setup() {
+  WiFi.mode(WIFI_OFF);
+#if DEBUG || RETRIEVE || DELETE
+  initSerial();
+#endif  // DEBUG || RETRIEVE || DELETE
+  initSI7021();
+  initFS();
+#if RETRIEVE
+  delay(5 * SEC);
+  retrieveData();
+#endif //RETRIEVE
+#if DELETE
+  delay(5 * SEC);
+  deleteData();
+#else
+  readSensor();
+  saveData();
+  sleep();
+#endif  // DELETE
+#if RETRIEVE || DELETE
+  quickSleep();
+#endif  // RETRIEVE || DELETE
+}
+
+void loop() {
+}
+
+void sleep(void) {
+#if DEBUG
+  Serial.print(F("Go to sleep for "));
+  Serial.print(WAKEUP_RATE * MICROSEC);
+  Serial.println(F("us."));
+#endif  //DEBUG
+  ESP.deepSleep(WAKEUP_RATE * MICROSEC);
+}
+
+void quickSleep(void) {
+  Serial.print(F("Go to sleep for "));
+  Serial.print(WAKEUP_RATE * MICROSEC);
+  Serial.println(F("us."));
+  ESP.deepSleep(1 * MICROSEC);
+}
+
---
+++ software design/ESP8266_data_logger/filesystem.ino
@@ -0,0 +1,53 @@
+/*
+    Copyright (C) 2016 Clément Ronzon
+
+    This file is part of EspDataLogger.
+
+    SpotPipe is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+void saveData(void) {
+  File file = SPIFFS.open(FILE_PATH, "a");
+  if (!file) {
+#if DEBUG
+    Serial.println(F("file open failed"));
+#endif  //DEBUG
+    return;
+  }
+  file.print(_celsiusHundredths); file.print(F(",")); file.println(_humidityPercent);
+  file.close();
+}
+
+void retrieveData(void) {
+  File file = SPIFFS.open(FILE_PATH, "r");
+  if (!file) {
+    Serial.println(F("file open failed"));
+    return;
+  }
+  Serial.println(F("-- bof --"));
+  while(file.available()) {
+    Serial.println(file.readStringUntil('\n'));
+  }
+  Serial.println(F("-- eof --"));
+}
+
+void deleteData(void) {
+  bool ok = SPIFFS.remove(FILE_PATH);
+  if (ok) {
+    Serial.print(FILE_PATH); Serial.println(F(" deleted"));
+  } else {
+    Serial.println(F("file delete failed"));
+  }
+}
+
